91d1d62b2b2d4193c2e93d2a0d51bf6a19d92763
ShivamMiguel <gongamiguel@gmail.com>
2025-08-11 09:57:46 +0100

    development config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,12 @@
-// @ts-check
 import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import globals from 'globals';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
+  ...tseslint.configs.recommended,
   {
     languageOptions: {
       globals: {
@@ -25,10 +21,19 @@ export default tseslint.config(
     },
   },
   {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+
+    ignorePatterns: ['dist/', 'node_modules/', '.eslintrc.js', 'eslint.config.mjs'],
+
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+
+      'prettier/prettier': 'error',
     },
   },
+  prettierConfig
 );
